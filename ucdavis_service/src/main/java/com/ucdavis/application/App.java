709f982f1ac1d336945063f75cc0293900454e3f
Roy <rygworkspace@GMAIL.COM>
2017-08-03 04:07:37 -0700

    add CIMIS API

--- ucdavis_service/src/main/java/com/ucdavis/application/App.java
+++ ucdavis_service/src/main/java/com/ucdavis/application/App.java
@@ -2,8 +2,9 @@ package com.ucdavis.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-
+@EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
 public class App {
 
