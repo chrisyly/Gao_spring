fa919fa1a32e06973d2f531851739c2d97dbcde6
Roy <rygworkspace@GMAIL.COM>
2017-08-21 23:02:22 -0700

    update the ucdavisService with readCSV and writeCSV.

--- ucdavis_service/src/main/java/com/ucdavis/application/Service/Main.java
+++ ucdavis_service/src/main/java/com/ucdavis/application/Service/Main.java
@@ -1,39 +1,31 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class Main {
 
-
 	public static void main(String args[]) throws JsonParseException, JsonMappingException, IOException, JSONException {
-		CimisService test = new CimisService();
+		UCDavisService davisService = new UCDavisService();
+		CimisService cimisService = new CimisService();
 		JsonParser parser = new JsonParser();
-		JSONObject jsonObject = test.processRequestQuery("http://et.water.ca.gov/api/data?appKey=4a3b5300-f248-4b6a-bb49-8368d1cac187&targets=2,8,127&startDate=2010-01-01&endDate=2010-01-02&dataItems=day-air-tmp-avg");
-		System.out.println(jsonObject.toString());
-
-		Map<String, String> mapping = parser.jsonStringToMap(jsonObject);
 		
-		Iterator<?> itr = mapping.keySet().iterator();
-		while (itr.hasNext()) {
-			String key = (String) itr.next();
-			//if (key.equals("Records")) {
-				//System.out.println(key + mapping.get(key).toString());
-			//}
+		// create & update the local csv data
+		String base = "http://et.water.ca.gov/api/data?appKey=4a3b5300-f248-4b6a-bb49-8368d1cac187&targets=";
+		String ext = "&startDate=2017-01-01&endDate=2017-01-10&dataItems=day-air-tmp-avg";
+		for (int i = 1; i <= 252; i++) {
+			JSONObject jsonObject = cimisService.processRequestQuery(base + i + ext);
+			List<JSONObject> records = parser.getListFromJson(parser.extractDataItems((parser.jsonStringToMap(jsonObject).get("Records"))));
+			davisService.writeToCSV(records, String.valueOf(i));
 		}
+		
+		// test csv readers
+		String csvPath = "2.csv";
+        System.out.println(davisService.getDataMapFromCSV(csvPath).toString());
 	}
 }
--- ucdavis_service/src/main/java/com/ucdavis/application/Service/UCDavisService.java
+++ ucdavis_service/src/main/java/com/ucdavis/application/Service/UCDavisService.java
@@ -2,6 +2,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -9,6 +13,8 @@ import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.opencsv.CSVReader;
 
 /**
  * 
@@ -32,29 +38,62 @@ public class UCDavisService {
 	
 	// save data into local Map:data
 	public void getDataFromList(List<JSONObject> records) {
-		// traverse the list
-		Map<String, String> recordMap = new HashMap<>();
-		for (JSONObject record : records) {
-			// find the key of recordMap, date & hour
-			String dateValue = record.get("Date");
-		}
-		UCDavisService.data.put(this.mapOfRequest.get("targets"), recordMap);
 	}
 	
-	// local CSV
-	public void getDataFromCSV(String csvPath) {
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(csvPath));
-			String line = null;
-			String cvsSplitBy = ",";
-			
-			
-		} catch (FileNotFoundException e) {
-			System.err.println("[Fail]Fail to find the file");
-			e.printStackTrace();
-		}
-		
-		
+	/**
+	 * get data mapping from local csv
+	 * @param csvPath
+	 * @return Map<String, Map<String, String>> targetAndData
+	 * Updated: Aug.21st, Mushi
+	 */
+	public Map<String, Map<String, String>> getDataMapFromCSV(String csvPath) {
+		Map<String, Map<String, String>> targetAndData = new HashMap<>();
+    	Map<String, String> DateAndTemp = new HashMap<>();
+    	
+		BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        boolean isFirstRow = true;
+        // initialize the item index
+        int indexOfDate = 0;
+		int indexOfHour = 0;
+		int indexOfTemp = 0;
+		int indexOfHlyTemp = 0;
+
+        try {
+            br = new BufferedReader(new FileReader(csvPath));
+            while ((line = br.readLine()) != null) {
+            	String[] items = line.split(cvsSplitBy);
+            	if (isFirstRow) {
+            		 indexOfDate = Arrays.asList(items).indexOf("Date");
+            		 indexOfHour = Arrays.asList(items).indexOf("Hour");
+            		 indexOfTemp = Arrays.asList(items).indexOf("DayAirTmpAvg");
+            		 indexOfHlyTemp = Arrays.asList(items).indexOf("HlyAirTmp");
+            		 isFirstRow = false;
+            		 continue;
+            	}
+            	// matching data and temperature
+            	if (indexOfHour == -1) { // daily
+					DateAndTemp.put(items[indexOfDate], items[indexOfTemp]);
+				} else { // hourly
+					DateAndTemp.put(items[indexOfDate] + ":" +items[indexOfHour] , items[indexOfHlyTemp]);
+				}
+            }
+            targetAndData.put(new File(csvPath).getName(), DateAndTemp);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return targetAndData;
 	}
 	
 	// TODO, database
@@ -64,9 +103,49 @@ public class UCDavisService {
 	
 	/**
 	 * write List<JSONObject> to CSV, file name is the key of map
+	 * return the activity of stations
+	 *  * Updated: Aug.21st, Mushi
 	 */
 	public void writeToCSV(List<JSONObject> records, String fileName) {
-		File file = new File("/" + fileName +".csv");
+		boolean isFirstRow = true;
+		// empty
+		StringBuilder csv = new StringBuilder();
+		for (JSONObject record : records) {
+			Iterator<?> itr = record.keys();
+			// Column not created yet
+			if (isFirstRow) {
+				Iterator<?> columns = record.keys();
+				while (columns.hasNext()) {
+					csv.append((String)columns.next());
+					csv.append(",");
+				}
+				csv.append("\n"); // first row item name done
+				isFirstRow = false;
+			}
+			
+			// get the values of all items
+			while (itr.hasNext()) {
+				String key = (String) itr.next();
+				try {
+					csv.append(record.get(key).toString());
+					csv.append(",");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+			csv.append("\n");
+		}
+		
+		// csv
+		PrintWriter pw;
+		try {
+			pw = new PrintWriter(new File(fileName+".csv"));
+			pw.write(csv.toString());
+	        pw.close();
+	        System.out.println("csv file: <" + fileName + "> created");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String calculateHeatUnitResult(String key) {
@@ -90,7 +169,6 @@ public class UCDavisService {
 			// find the end of value of targets
 			int i = index;
 			while (i < request.length() && Character.isDigit(request.charAt(i))) i++;
-			System.out.println(i);
 			queryMap.put("targets", request.substring(index, i));
 		}
 		
